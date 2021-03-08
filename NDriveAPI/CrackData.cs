05a44131df9d9938bb15836d4d70445d65b870fb
Nitesh Shrestha <nitesh.shresthax@gmail.com>
2021-03-08 14:18:49 +0545

    Add project files.

---
+++ NDriveAPI/CrackData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NDriveAPI
+{
+    public class CrackData
+    {
+        public string GameName { get; set; }
+        public DateTime CrackDate { get; set; }
+        public bool Cracked => DateTime.Now > CrackDate;
+    }
+}
